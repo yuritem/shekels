0c259f636c317777f0f792b63704c1ea6626d7a5
yuritem <yuritemiraev@gmail.com>
2024-04-06 17:49:28 +0300

    Manually create event loop in bot.main

--- bot/main.py
+++ bot/main.py
@@ -55,4 +55,6 @@ async def main():
 
 if __name__ == "__main__":
     setup_logging(config=config)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     asyncio.get_event_loop().run_until_complete(main())
