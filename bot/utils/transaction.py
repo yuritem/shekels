0a244a42d89df0bb13cf3bf8bec9f4a9871571b3
yuritem <yuritemiraev@gmail.com>
2024-04-12 23:14:34 +0300

    Change bot.utils.transaction.split_transaction return signature

--- bot/utils/transaction.py
+++ bot/utils/transaction.py
@@ -1,35 +1,41 @@
 from datetime import datetime
-from typing import Iterator
 
 from dateutil.relativedelta import relativedelta
 
 
-async def split_transaction(amount_total: float, months: int) -> Iterator[tuple]:
+def split_float_conserve_sum(money: float, parts: int) -> list[float]:
+    """
+        Splits a float `money` into `parts` parts, conserving the sum.
+        Assumes `money` is rounded to 2 decimal places. If not, rounds it.
+        Returns a list of floats that sum to `money`.
+    """
+    sign = 1 if money > 0 else -1  # sign treated separately for proper integer division and modulo
+    money = abs(money)
+    money = round(money, 2)
+    money_cents = int(money * 100)
+    part_cents, remaining_cents = divmod(money_cents, parts)
+    return [
+        sign * round((part_cents + int(i < remaining_cents)) / 100, 2)
+        for i in range(parts)
+    ]
+
+
+async def split_transaction(amount_total: float, months: int, start_date: datetime = None) -> tuple[list[datetime], list[float]]:
     """
         Meant for installment payments.
         For `months` > 1 splits transaction into multiple transactions 1 month apart with `amount_total` equally
     distributed between payments and calculates each transaction's `timestamp`.
-        Starting timestamp is taken as the current time calculated inside this function.
-        Returns an iterator of timestamps and amounts.
+        If not provided, starting timestamp is taken as the current time calculated inside this function.
+        Returns a tuple of two lists: timestamps and amounts.
     """
 
-    # sign treated separately for proper integer division and modulo
-    sign = 1 if amount_total > 0 else -1
-    amount_total = abs(amount_total)
+    amounts = split_float_conserve_sum(amount_total, months)
 
-    amount_total_cents = int(round(amount_total * 100, 0))
-    amount_cents = amount_total_cents // months
-    remaining_cents = amount_total_cents % months
-
-    amounts_cents = [amount_cents for _ in range(months)]
-    for i in range(remaining_cents):
-        amounts_cents[i] += 1
-    amounts = [sign * amnt / 100 for amnt in amounts_cents]
-
-    now = datetime.now()
+    if not start_date:
+        start_date = datetime.now()
     timestamps = [
-        now + relativedelta(months=+i)
+        start_date + relativedelta(months=+i)
         for i in range(months)
     ]
 
-    return zip(timestamps, amounts)
+    return timestamps, amounts
