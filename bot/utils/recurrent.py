355ef4942da541f6e67728c435ecf1560e2748e3
yuritem <yuritemiraev@gmail.com>
2024-04-13 01:09:13 +0300

    add timezone in bot.utils.recurrent.recurrent_timestamps

--- bot/utils/recurrent.py
+++ bot/utils/recurrent.py
@@ -7,16 +7,21 @@ from bot.db.types import RecurrentPeriodUnit
 def recurrent_timestamps(
         next_timestamp: datetime,
         period: int,
-        period_unit: RecurrentPeriodUnit
+        period_unit: RecurrentPeriodUnit,
+        up_to_timestamp: datetime = None
 ) -> tuple[list[datetime], datetime]:
     """
     Returns a tuple of:
-        - list of timestamps starting from `next_timestamp` up to `now` with given periodicity;
-        - next timestamp after `now`.
+        - list of timestamps starting from `next_timestamp` up to `up_to_timestamp` with given periodicity;
+        - next timestamp after `up_to`.
     """
     relativedelta_kwargs = {f"{period_unit}s": period}
-    now = datetime.now()
+    if not up_to_timestamp:
+        up_to_timestamp = datetime.now(tz=next_timestamp.tzinfo)
+    else:
+        if not up_to_timestamp.tzinfo:
+            up_to_timestamp = up_to_timestamp.replace(tzinfo=next_timestamp.tzinfo)
     timestamps = [next_timestamp]
-    while timestamps[-1] <= now:
+    while timestamps[-1] <= up_to_timestamp:
         timestamps.append(timestamps[-1] + relativedelta(**relativedelta_kwargs))
     return timestamps[:-1], timestamps[-1]
